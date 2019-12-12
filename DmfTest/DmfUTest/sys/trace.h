995376e46fc0c5b4d9a606e2a76137454bd6dd17
Anoop Kurungod <ankurung@microsoft.com>
2019-12-12 11:37:57 -0800

    DmfUTest project cleanup

--- DmfTest/DmfUTest/sys/trace.h
+++ DmfTest/DmfUTest/sys/trace.h
@@ -19,30 +19,52 @@ Environment:
 #pragma once
 
 // Define the tracing flags.
-// Tracing GUIDs:
 //
-// Client Driver - {93D98F10-5DB5-4CB3-A57A-81CB4D73A81D}
-//
-// NOTE: Every driver must define a unique GUID otherwise tracing from multiple drivers
-//       that use the same GUID will appear.
-//
-// DMF           - {E3F037DA-C707-4501-A08B-2F0887871477}
+// DmfUTest Tracing GUID - {EAF8F31E-30E1-499B-ABEB-8E6B051E1A75}
+// DMF Tracing GUID - {E028F9D9-8BD5-494F-B288-CF1C049F4BD1}
 //
 
 #define WPP_CONTROL_GUIDS                                                               \
    WPP_DEFINE_CONTROL_GUID(                                                             \
-        DmfTraceGuid, (E3F037DA,C707,4501,A08B,2F0887871477),                           \
+        DmfTraceGuid, (E028F9D9,8BD5,494F,B288,CF1C049F4BD1),                           \
         WPP_DEFINE_BIT(DMF_TRACE)                                                       \
         )                                                                               \
     WPP_DEFINE_CONTROL_GUID(                                                            \
-        VHidMini2DmfU, (93D98F10,5DB5,4CB3,A57A,81CB4D73A81D),                          \
+        DmfUTestDriverTraceGuid, (EAF8F31E,30E1,499B,ABEB,8E6B051E1A75),                \
         WPP_DEFINE_BIT(MYDRIVER_ALL_INFO)                                               \
         WPP_DEFINE_BIT(TRACE_DEVICE)                                                    \
         WPP_DEFINE_BIT(TRACE_CALLBACK)                                                  \
         )                                                                               \
 
-#define WPP_LEVEL_FLAGS_LOGGER(lvl,flags) WPP_LEVEL_LOGGER(flags)
-#define WPP_LEVEL_FLAGS_ENABLED(lvl, flags) (WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level  >= lvl)
+//
+// This comment block is scanned by the trace preprocessor to define our
+// Trace function.
+//
+// USEPREFIX and USESUFFIX strip all trailing whitespace, so we need to surround
+// FuncExit messages with brackets 
+//
+// begin_wpp config
+// FUNC Trace{FLAG=MYDRIVER_ALL_INFO}(LEVEL, MSG, ...);
+// FUNC TraceEvents(LEVEL, FLAGS, MSG, ...);
+// FUNC FuncEntry{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
+// FUNC FuncEntryArguments{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExit{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExitVoid{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
+// FUNC TraceError{LEVEL=TRACE_LEVEL_ERROR}(FLAGS, MSG, ...);
+// FUNC TraceInformation{LEVEL=TRACE_LEVEL_INFORMATION}(FLAGS, MSG, ...);
+// FUNC TraceVerbose{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExitNoReturn{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
+// USEPREFIX(FuncEntry, "%!STDPREFIX! [%!FUNC!] --> Entry");
+// USEPREFIX(FuncEntryArguments, "%!STDPREFIX! [%!FUNC!] --> Entry");
+// USEPREFIX(FuncExit, "%!STDPREFIX! [%!FUNC!] <-- Exit <");
+// USESUFFIX(FuncExit, ">");
+// USEPREFIX(FuncExitVoid, "%!STDPREFIX! [%!FUNC!] <-- Exit");
+// USEPREFIX(TraceError, "%!STDPREFIX! [%!FUNC!] ERROR:");
+// USEPREFIX(TraceEvents, "%!STDPREFIX! [%!FUNC!]");
+// USEPREFIX(TraceInformation, "%!STDPREFIX! [%!FUNC!]");
+// USEPREFIX(TraceVerbose, "%!STDPREFIX! [%!FUNC!]");
+// USEPREFIX(FuncExitNoReturn, "%!STDPREFIX! [%!FUNC!] <--");
+// end_wpp
 
 // eof: Trace.h
 //
