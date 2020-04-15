9525df4d5300c7eea9c15034a6ac268c5f7f09d6
Sam Tertzakian <32625754+samtertzakian@users.noreply.github.com>
2020-04-14 17:33:01 -0700

    Fix SAL issue in sample.

--- Dmf/Modules.Template/Dmf_SampleInterfaceUpperTransport2.c
+++ Dmf/Modules.Template/Dmf_SampleInterfaceUpperTransport2.c
@@ -494,7 +494,6 @@ Return Value:
 #pragma code_seg("PAGE")
 _Function_class_(DMF_ChildModulesAdd)
 _IRQL_requires_max_(PASSIVE_LEVEL)
-_Must_inspect_result_
 static
 VOID
 DMF_SampleInterfaceUpperTransport2_ChildModulesAdd(
