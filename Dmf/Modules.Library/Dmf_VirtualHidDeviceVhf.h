23137085852effe0597b508efc87f0646a88986e
Jon Campbell <joncamp@microsoft.com>
2020-09-03 09:25:16 -0700

    Fix error in HID_LOGICAL_MAXIMUM_DWORD macro
    Was incorrectly using MINIMUM instead of MAXIMUM

--- Dmf/Modules.Library/Dmf_VirtualHidDeviceVhf.h
+++ Dmf/Modules.Library/Dmf_VirtualHidDeviceVhf.h
@@ -1141,7 +1141,7 @@ Environment:
 #define HID_LOGICAL_MINIMUM_WORD(val)           (HID_GLOBAL_LOGICAL_MINIMUM | HID_SIZE_WORD), (val & 0xFF), ((val >> 8) & 0xFF)
 #define HID_LOGICAL_MAXIMUM_WORD(val)           (HID_GLOBAL_LOGICAL_MAXIMUM | HID_SIZE_WORD), (val & 0xFF), ((val >> 8) & 0xFF)
 #define HID_LOGICAL_MINIMUM_DWORD(val)          (HID_GLOBAL_LOGICAL_MINIMUM | HID_SIZE_DWORD), (val & 0xFF), ((val >> 8) & 0xFF), ((val >> 16) & 0xFF), ((val >> 24) & 0xFF)
-#define HID_LOGICAL_MAXIMUM_DWORD(val)          (HID_GLOBAL_LOGICAL_MINIMUM | HID_SIZE_DWORD), (val & 0xFF), ((val >> 8) & 0xFF), ((val >> 16) & 0xFF), ((val >> 24) & 0xFF)
+#define HID_LOGICAL_MAXIMUM_DWORD(val)          (HID_GLOBAL_LOGICAL_MAXIMUM | HID_SIZE_DWORD), (val & 0xFF), ((val >> 8) & 0xFF), ((val >> 16) & 0xFF), ((val >> 24) & 0xFF)
 
 // This Module is only supported in Kernel-mode because VHF only support Kernel-mode.
 //
