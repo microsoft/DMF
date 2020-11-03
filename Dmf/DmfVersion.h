6016fe9d426b1d0a26112f0d4c66fafc60974c14
Sam Tertzakian <32625754+samtertzakian@users.noreply.github.com>
2020-11-02 17:23:33 -0800

    1. Update documentation. (#142)
    2. Add version file so build systems can detect release information.

---
+++ Dmf/DmfVersion.h
@@ -0,0 +1,11 @@
+// DmfVersion.h
+//
+// Contains DMF version information. In the future, this file may hold information that is embedded in binary images
+// built using DMF.
+//
+
+// DMF Release: v1.1.77
+//
+
+// eof: DmfVersion.h
+//
