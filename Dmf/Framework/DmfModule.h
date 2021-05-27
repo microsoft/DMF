75c0345debc32013605c7726e1ef31f8225c8979
Sam Tertzakian <32625754+samtertzakian@users.noreply.github.com>
2021-05-27 14:50:02 -0700

    Merge20210527.1 (#165)
    Fix regression caused by previous PR (v1.1.88).

--- Dmf/Framework/DmfModule.h
+++ Dmf/Framework/DmfModule.h
@@ -906,17 +906,6 @@ DMF_HandleValidate_ClosingOk(
               (DmfAssert(FALSE)) :                                                              \
               (DMF_HandleValidate_ClosingOk(ModuleHandle))                                      \
 
-VOID
-DMF_HandleValidate_IsCreated(
-    _In_ DMFMODULE DmfModule
-    );
-
-#define DMFMODULE_VALIDATE_IN_METHOD_IS_CREATED(ModuleHandle, ModuleType)                       \
-                                                                                                \
-    (! WdfObjectIsCustomType(ModuleHandle, DMF_##ModuleType)) ?                                 \
-              (DmfAssert(FALSE)) :                                                              \
-              (DMF_HandleValidate_IsCreated(ModuleHandle))                                      \
-
 __forceinline
 DMFMODULE
 DMFMODULEVOID_TO_MODULE(
